80598545b2ff85de3f19698f00a08b3a2507cb93
Javier Collao <jav.acollao@gmail.com>
2020-10-08 20:49:59 -0300

    new index

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,14 +11,19 @@ const border = document.querySelector(".border");
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
+console.log(header_height)
+console.log(section_height)
+
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
+     
     
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll * speed}px)`;
     });
+   
 
     opacity.forEach(element => {
         element.style.opacity = scroll / (sectionY.top + section_height);
